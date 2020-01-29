e877b732bc1f13fa1447f697c38ce36ed7a27d10
StevenZelenak <stevenmzelenak@gmail.com>
2020-01-28 19:07:47 -0800

    added array of ducks

--- main.js
+++ main.js
@@ -1,1 +1,110 @@
-console.log("hell");
+//filter by rubber, color, gender
+//multiple filters at once if possible
+
+const ducks = [
+
+    {
+        isRubber: true,
+        color: 'pink',
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'single pringle',
+        diet: 'meat',
+        age: 25,
+        featherNum: 100,
+        name: 'Regina',
+        imageURL: "https://cdn.shopify.com/s/files/1/0604/4801/products/IMG_2533_clipped_rev_1-min_1024x1024.jpeg?v=1506030781"
+    },
+    {
+        isRubber: true,
+        color: 'blue',
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'ready to mingle',
+        diet: 'vegan',
+        age: 47,
+        featherNum: 0,
+        name: 'Regiii',
+        imageURL: "https://m.media-amazon.com/images/I/51WEvgpXwYL._SR500,500_.jpg"
+    },
+    {
+        isRubber: false,
+        color: 'green',
+        gender: 'female',
+        isMigrator: false,
+        socialStatus: 'not ready yet',
+        diet: 'crackers',
+        age: 30,
+        featherNum: 42,
+        name: 'Tom',
+        imageURL: "https://images.homedepot-static.com/productImages/c938f2d3-6474-4400-98ff-327f7b71f74e/svn/amscan-seasonal-decorations-399485-64_1000.jpg"
+    },
+    {
+        isRubber: true,
+        color: 'pink',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'hot stuff',
+        diet: 'bread',
+        age: 55,
+        featherNum: 5,
+        name: 'Denver',
+        imageURL: "https://cdn.shopify.com/s/files/1/0604/4801/products/IMG_2533_clipped_rev_1-min_1024x1024.jpeg?v=1506030781"
+    },
+    {
+        isRubber: false,
+        color: 'blue',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'coexist',
+        diet: 'Humans',
+        age: 100,
+        featherNum: 1000,
+        name: 'Duck God',
+        imageURL: "https://m.media-amazon.com/images/I/51WEvgpXwYL._SR500,500_.jpg"
+    },
+    {
+        isRubber: true,
+        color: 'green',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'fight me bitch',
+        diet: 'vegan',
+        age: 15,
+        featherNum: 150,
+        name: 'Tiffany',
+        imageURL: "https://images.homedepot-static.com/productImages/c938f2d3-6474-4400-98ff-327f7b71f74e/svn/amscan-seasonal-decorations-399485-64_1000.jpg"
+    },
+    {
+        isRubber: false,
+        color: 'pink',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'war, war never ends',
+        diet: 'nothing',
+        age: 35,
+        featherNum: 4,
+        name: 'Ryan',
+        imageURL: "https://cdn.shopify.com/s/files/1/0604/4801/products/IMG_2533_clipped_rev_1-min_1024x1024.jpeg?v=1506030781"
+    },
+    {
+        isRubber: true,
+        color: 'blue',
+        gender: 'male',
+        isMigrator: false,
+        socialStatus: 'ready to tingle',
+        diet: 'fairies',
+        age: 1000,
+        featherNum: 90,
+        name: 'Reg the fairy eater',
+        imageURL: "https://m.media-amazon.com/images/I/51WEvgpXwYL._SR500,500_.jpg"
+    },
+
+
+
+];
+
+console.log(ducks);
+const outputDom = () =>{
+
+}
